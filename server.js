9b5f2855dae24a2f9be65ec6b8d2b557cf72391c
Chris Mitsopoulos <chrismits@Chriss-MBP.fios-router.home>
2019-10-31 16:21:59 -0400

    Initial recleagues setup

---
+++ server.js
@@ -0,0 +1,34 @@
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = require('dotenv')
+    dotenv.config()
+}
+
+
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+//database connections
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
+//import router
+const indexRouter = require('./routes/index')
+
+//Configure express app 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public')) //images and other static files
+
+app.use('/', indexRouter)
+
+
+app.listen(process.env.PORT)
+
