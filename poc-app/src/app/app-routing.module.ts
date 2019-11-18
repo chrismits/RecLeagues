a8547f6c007e01215dcb319cd8fb6045b1e70032
georgewest13 <gwest2016@spyponders.com>
2019-11-17 22:59:51 -0500

    Made it more tufts-like

--- poc-app/src/app/app-routing.module.ts
+++ poc-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeagueComponent } from './league/league.component';
+import { TeamComponent } from './team/team.component';
+import { PlayerComponent } from './player/player.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'leagues', component: LeagueComponent},	
+	{ path: 'teams', component: TeamComponent},	
+	{ path: 'players', component: PlayerComponent}	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- poc-app/src/app/app.component.ts
+++ poc-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poc-app';
+  title = 'RecLeagues';
 }
