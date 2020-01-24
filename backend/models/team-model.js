0e59c76086ebe65bb4f43ea826ebf801aad14a70
Chris Mitsopoulos <mitsopouloschris@gmail.com>
2020-01-24 15:15:27 -0500

    Updated entries and validation for Team db model.

--- backend/models/team-model.js
+++ backend/models/team-model.js
@@ -1,30 +1,51 @@
 /* 
 Database schema for Team
-Fixes: 
-    - Validation for captain?
-    - Validation for league
-    - Insert sport as a field?
+Notes: 
+    - Does captain requires a validation function?
+    - Add entry for picture of logo. Store as buffer or link to object store.
+
 */
 
 var teamSchema = new mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-    name: {type: String, required: true, minlength: 1, maxlength: 30},
-    logo_loc: {type: String, required: false}, //Location of logo image in Object Store?
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true
+    },
+    name: {
+        type: String, 
+        required: true, 
+        minlength: 1, 
+        maxlength: 30
+    },
     size: {
         type: Number, 
         required: true, 
         validate: {
             validator: function(v) {
-                return v >= 1
+                return v >= 1 && v < 30
             },
-            message: 'Team size should be at least 1'
+            message: 'Team size should be 1 - 30 players'
         }
     },
-    captain: {type: mongoose.Schema.Types.ObjectId, ref: 'Player', default: undefined},
-    players: [{type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
-    league: {type: mongoose.Schema.Types.ObjectId, ref: 'Leage'},
-    created: {type: Date, default: Date.now}
+    captain: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Player', 
+        default: undefined,
+    },
+    players: [{ // Array of players
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Player'
+    }],
+    league: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'League'
+    },
+    created: {
+        type: Date, 
+        default: Date.now
+    }
 })
+
 
 var Team = mongoose.model('Team', teamSchema);
 module.exports = Team;
