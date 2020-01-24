bba3ff3e49b14eff781a18493eb7554f62e99caa
Chris Mitsopoulos <chrismits@Chriss-MacBook-Pro.local>
2020-01-24 14:41:40 -0500

    Added new fields to Player Database model. Updated validation methods.

--- backend/models/player-model.js
+++ backend/models/player-model.js
@@ -1,23 +1,60 @@
 /* 
-Database schema for player
-Fixes: 
-    - Validation
+Mongoose schema for Player. Exports Player model
+Notes: 
+    - Test cell-phone validator function
+    - Added reference field for team
 */
 
 var mongoose = require('mongoose')
 require('mongoose-type-email');
 
 var playerSchema = new mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-    first: {type: String, required: true, minlength:1},
-    last: {type: String, required: true, minlength:1},
-    cell: {type: String, required: false, default: undefined},
-    email: {type: mongoose.SchemaTypes.Email, required: true},
-    waiver: {type: Boolean, default: false},
-    created: {type: Date, default: Date.now}
+    _id: {
+        type: mongoose.Schema.Types.ObjectId, 
+        auto: true 
+    },
+    first: {
+        type: String, 
+        required: true,
+        trim: true,
+        minlength: [2, 'DB Error: First name too short!']
+    },
+    last: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: [2, 'DB Error: Last name too short!']
+    },
+    cell: {
+        type: String, 
+        required: false,
+        default: '',
+        validate: {
+            validator: function(c) {
+                return /\d{10}/.test(c); // test this
+            }
+        }
+    },
+    email: {
+        type: mongoose.SchemaTypes.Email, 
+        required: true
+    },
+    signedWaiver: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date, 
+        default: Date.now
+    },
+    team: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team',
+        default: undefined
+    }
 })
 
-var Player = mongoose.model('Player', playerSchema);
+const Player = mongoose.model('Player', playerSchema);
 module.exports = Player;
 
 
