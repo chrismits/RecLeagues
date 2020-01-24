77c8211bd6f049a995fb835cf4149a4693db4b10
Chris Mitsopoulos <mitsopouloschris@gmail.com>
2020-01-24 16:17:40 -0500

    Updated Match db model and validation. Added sport and score entries

--- backend/models/match-model.js
+++ backend/models/match-model.js
@@ -1,16 +1,43 @@
 /* 
 Database schema for match
-Fixes:
-    - Custom validator for scoring: Figure out ideal representation.
+Notes:
+    - Changed score representation to have home and away scores as sub-fields.
+    - Added sport field. Update enum values to add more sports.
 */
 
 var matchSchema = new mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-    date: {type: Date, required: true},
-    home: {type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true},
-    away: {type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true},
-    score: {type: String, required: false},
-    created: {type: Date, default: Date.now}
+    _id: {
+        type: mongoose.Schema.Types.ObjectId, 
+        auto: true
+    },
+    date: {
+        type: Date, 
+        required: true
+    },
+    home: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team', 
+        required: true
+    },
+    away: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team', 
+        required: true
+    },
+    score: {
+        home_score: String,
+        away_score: String,
+        required: false
+    },
+    sport: {
+        type: String,
+        enum: ['Basketball', 'Soccer', 'Tennis', 'Other'],
+        default: 'Other'
+    },
+    created: {
+        type: Date, 
+        default: Date.now
+    }
 })
 
 var Match = mongoose.model('Match', matchSchema);
